859505a2b6b030125fe380fc7bec16682554d320
Kentaro Kuribayashi <kentarok@gmail.com>
2009-02-14 23:16:34 +0900

    DBIx::Ricoをpmsetupした。

---
+++ lib/DBIx/Rico.pm
@@ -0,0 +1,76 @@
+package DBIx::Rico;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+sub new {}
+
+1;
+
+__END__
+
+=head1 NAME
+
+DBIx::Rico - Yet, yet, ... another ORM for Perl
+
+=head1 SYNOPSIS
+
+  use DBIx::Rico;
+
+=head1 DESCRIPTION
+
+DBIx::Rico is  blah, blah, blah...
+
+=head1 METHODS
+
+=head2 new ( I<$args> )
+
+=over 4
+
+  my $obj = DBIx::Rico->new($args);
+
+Creates and returns a new DBIx::Rico object.
+
+=back
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * item
+
+Desctiption for the item above.
+
+=back
+
+=head1 AUTHOR
+
+Kentaro Kuribayashi E<lt>kentaro@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 COPYRIGHT AND LICENSE (The MIT License)
+
+Copyright (c) Kentaro Kuribayashi E<lt>kentaro@cpan.orgE<gt>
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+=cut
